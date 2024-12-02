bac4bd774a4654c2da950cbecc4aaeb38145fa0f
cloosafm <alexandra.cloos@gmail.com>
2024-12-02 16:29:58 +0100

    outputting external ip

--- terraform/main.tf
+++ terraform/main.tf
@@ -76,7 +76,7 @@ resource "google_compute_firewall" "allow-ssh" {
 
 
 output "ip" {
-  value = google_compute_instance.my_instance.network_interface.0.network_ip
+  value = google_compute_instance.my_instance.network_interface.0.access_config.0.nat_ip
 }
 
 
