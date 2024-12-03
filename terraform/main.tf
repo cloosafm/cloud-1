6c7fd37d0887988e4510e9fc59d5e06e0fc72bb9
cloosafm <alexandra.cloos@gmail.com>
2024-12-03 16:17:29 +0100

    nedd to have role directories at root of ansible somehow. also, started to create 'dummy' files to explain how to use sensitive data

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "6.12.0"
     }
   }
@@ -15,9 +15,9 @@ provider "google" {
 }
 
 resource "google_compute_instance" "my_instance" {
-  name         = var.instance_info.name
-  machine_type = var.instance_info.machine_type
-  zone         = var.instance_info.zone
+  name                      = var.instance_info.name
+  machine_type              = var.instance_info.machine_type
+  zone                      = var.instance_info.zone
   allow_stopping_for_update = var.instance_info.allow_stopping_for_update
 
   tags = ["allow-ssh"] // optional, can be used to determine if this is instance is for dev, test, prod, etc
@@ -31,9 +31,9 @@ resource "google_compute_instance" "my_instance" {
   }
 
   network_interface {
-    network = google_compute_network.terraform_network.self_link
+    network    = google_compute_network.terraform_network.self_link
     subnetwork = google_compute_subnetwork.terraform_subnet.self_link
-    access_config { 
+    access_config {
       // needed even if empty
     }
   }
@@ -43,31 +43,44 @@ resource "google_compute_instance" "my_instance" {
   }
 
   provisioner "remote-exec" {
-    inline = ["echo 'Hello World'"]
+    inline = [
+      "echo '\\033[1;32mWelcome to Cloud-1\\033[0m'",
+      "echo 'Instance is up and running'",
+      "bash -c 'echo Current user: $(whoami)'",
+      "bash -c 'echo Current directory: $(pwd)'",
+      "echo 'Instance IP: ${self.network_interface.0.access_config.0.nat_ip}'"
+    ]
 
     connection {
-        host        = google_compute_instance.my_instance.network_interface.0.access_config.0.nat_ip
-        type        = "ssh"
-        user        = "${var.ssh_user}"
-        private_key = "${file("${var.ssh_priv_key_path}")}"
+      host        = google_compute_instance.my_instance.network_interface.0.access_config.0.nat_ip
+      type        = "ssh"
+      user        = var.ssh_user
+      private_key = file("${var.ssh_priv_key_path}")
     }
   }
-
-  provisioner "local-exec" {  
-    command = "ansible-playbook -i '${google_compute_instance.my_instance.network_interface.0.access_config.0.nat_ip},' --private-key ${var.ssh_priv_key_path} ../ansible/playbook.yml"
+}
+resource "null_resource" "ssh_key_management" {
+  provisioner "local-exec" {
+    command = <<EOT
+    ssh-keygen -R ${google_compute_instance.my_instance.network_interface.0.access_config.0.nat_ip} &&
+    ssh-keyscan -H ${google_compute_instance.my_instance.network_interface.0.access_config.0.nat_ip} >> ~/.ssh/known_hosts &&
+    ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i '${google_compute_instance.my_instance.network_interface.0.access_config.0.nat_ip},' --private-key ${var.ssh_priv_key_path} ../ansible/playbook.yml
+    EOT
   }
+
+  depends_on = [google_compute_instance.my_instance]
 }
 
-resource "google_compute_network" "terraform_network"{
-    name = var.tf_network_info.name
-    auto_create_subnetworks = var.tf_network_info.auto_create_subnetworks
+resource "google_compute_network" "terraform_network" {
+  name                    = var.tf_network_info.name
+  auto_create_subnetworks = var.tf_network_info.auto_create_subnetworks
 }
 
 resource "google_compute_subnetwork" "terraform_subnet" {
-    name = var.tf_subnet_info.name
-    ip_cidr_range = var.tf_subnet_info.ip_cidr_range
-    region = var.tf_subnet_info.region
-    network = google_compute_network.terraform_network.id
+  name          = var.tf_subnet_info.name
+  ip_cidr_range = var.tf_subnet_info.ip_cidr_range
+  region        = var.tf_subnet_info.region
+  network       = google_compute_network.terraform_network.id
 }
 
 resource "google_compute_firewall" "allow-ssh" {
@@ -83,7 +96,7 @@ resource "google_compute_firewall" "allow-ssh" {
     ports    = ["22"]
   }
 
-  source_tags = ["web"]
+  source_tags   = ["web"]
   source_ranges = ["0.0.0.0/0"]
-  target_tags = ["allow-ssh"] 
+  target_tags   = ["allow-ssh"]
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -17,52 +17,52 @@ variable "ssh_priv_key_path" {
 }
 
 variable "region" {
-    default = "us-west1"
+  default = "us-west1"
 }
 
 variable "zone" {
-    default = "us-west1-c"
+  default = "us-west1-c"
 }
 
 variable "os-image" {
-    default = "ubuntu-os-cloud/ubuntu-2204-lts"
+  default = "ubuntu-os-cloud/ubuntu-2204-lts"
 }
 
 variable "instance_info" {
-    type = object({
-      name = string
-      machine_type = string
-      zone = string
-      allow_stopping_for_update = bool
-    })
-    default = {
-      name = "terraform-instance"
-      machine_type = "e2-micro"
-      zone = "us-west1-a"
-      allow_stopping_for_update = true
-    }
+  type = object({
+    name                      = string
+    machine_type              = string
+    zone                      = string
+    allow_stopping_for_update = bool
+  })
+  default = {
+    name                      = "terraform-instance"
+    machine_type              = "e2-micro"
+    zone                      = "us-west1-a"
+    allow_stopping_for_update = true
+  }
 }
 
 variable "tf_network_info" {
-    type = object({
-      name = string
-      auto_create_subnetworks = bool
-    })
-    default = {
-      name = "terraform-network"
-      auto_create_subnetworks = false
-    }
+  type = object({
+    name                    = string
+    auto_create_subnetworks = bool
+  })
+  default = {
+    name                    = "terraform-network"
+    auto_create_subnetworks = false
+  }
 }
 
 variable "tf_subnet_info" {
-    type = object({
-      name = string
-      ip_cidr_range = string
-      region = string
-    })
-    default = {
-      name = "terraform-subnetwork"
-      ip_cidr_range = "10.20.0.0/16"
-      region = "us-west1"
-    }
+  type = object({
+    name          = string
+    ip_cidr_range = string
+    region        = string
+  })
+  default = {
+    name          = "terraform-subnetwork"
+    ip_cidr_range = "10.20.0.0/16"
+    region        = "us-west1"
+  }
 }
